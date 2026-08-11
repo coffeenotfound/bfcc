c702e953d68c90bc993158025b69587be27be5b4
Jan Katzer <jan@katzer.dev>
2026-08-11 21:00:10 +0200

    In boilerplate.c make outb a local array and don't free tape at the end

--- boilerplate.c
+++ boilerplate.c
@@ -5,20 +5,19 @@
 typedef unsigned char uchar;
 typedef unsigned int uint;
 
-#define NUM_CELLS (32*1024)
+#define NUM_CELLS (64*1024)
 
 void print_now(uchar* o, uint buf_len) {
 	fwrite(o, 1, buf_len, stdout);
 }
 
 int main() {
-	uchar* outb = calloc(/*OUT_BUF_LEN*/, 1);
+	uchar outb[/*OUT_BUF_LEN*/] = {0};
 	uchar* tape = calloc(NUM_CELLS, 1);
 	uchar* h = tape;
 	
 	/*MAIN_CODE*/
 	
-	free(tape);
-	free(outb);
+	// don't free tape, let it be reclaimed by process exit
 	return 0;
 }
